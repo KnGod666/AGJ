a214076e5d28d801aa14c96984415e2dd3bfd030
KnGod666 <ianrvr@estudiantes.uci.cu>
2024-03-02 11:30:12 -0500

    made test level, basic implementation of the main mechanic

--- Scripts/player_movement.gd
+++ Scripts/player_movement.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
 @export var player_node :Node2D
-
+@onready var raycast = $"../../RayCast2D"
+var locked = false
+var enabled = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -12,7 +14,24 @@ func _process(delta):
 	pass
 
 func _input(event):
-	var i = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
-	if i != Vector2(0,0):
-		player_node.create_tween().tween_property(player_node,"position",player_node.position+i*100,0.2)
+	if enabled:
+		var i = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+		if i != Vector2(0,0) and not locked:
+			locked = true
+			if i.x != 0 and i.y !=0:
+				i.y = 0
+			raycast.target_position = i*100
+			raycast.force_raycast_update()
+			if not raycast.is_colliding():
+				player_node.create_tween().tween_property(player_node,"position",player_node.position+i*100,0.2).finished.connect(unlock)
+			else:
+				locked = false
 	pass
+
+func unlock():
+	locked = false
+
+
+func _on_tp_tool_tp_enabled(enabled):
+	self.enabled = not enabled	#could give trouble in the future if set during movement
+	pass # Replace with function body.
---
+++ Scripts/tp_tool.gd
@@ -0,0 +1,54 @@
+extends Node2D
+
+signal tp_enabled
+
+@export var player_node :Node2D
+@onready var tp_probe = $TelepointChooser
+var locked = false
+var enabled = false
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _input(event):
+	if enabled:
+		var i = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+		if i != Vector2(0,0) and not locked:
+			#locked = true
+			if i.x != 0 and i.y !=0:
+				i.y = 0
+			position += i*100
+				#create_tween().tween_property(self,"position",position+i*100,0.2).finished.connect(unlock)
+			#else:
+			#	locked = false
+		if Input.is_action_pressed("TP_confirm"):
+			var tp_list = []
+			for x in range(-100,200,100):
+				for y in range(-100,200,100):
+					tp_probe.position = Vector2(x,y)
+					tp_probe.force_raycast_update()
+					if not (tp_probe.is_colliding()):
+						tp_list.push_back(Vector2(x,y))
+					pass
+			if tp_list.size() > 0:
+				var tp_target = to_global(tp_list[randi_range(0,tp_list.size()-1)])
+				#tp_target -= Vector2(50,50)
+				print(tp_target)
+				player_node.position =	tp_target
+				switch_enabled()
+	if(event.is_action_pressed("TP_enable")):
+		switch_enabled()
+	pass
+
+func switch_enabled():
+	position = Vector2(0,0)
+	enabled = not enabled;
+	visible = enabled
+	tp_enabled.emit(enabled)
+func unlock():
+	locked = false
